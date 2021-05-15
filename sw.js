3216404d9282d2675fcfc3b36ddd6dde63ba0a07
Yue Zhuo <y1zhuo@ucsd.edu>
2021-05-15 15:30:31 -0700

    Lab7

--- sw.js
+++ sw.js
@@ -2,43 +2,43 @@
 
 // You will need 3 event listeners:
 //   - One for installation
-const CACHE_NAME = 'lab7-cache';
-const urlsToCache = [
-  '/',
-  '/style.css',
-  '/scripts/script.js',
-  '/scripts/router.js',
-  '/components/entry-page.js',
-  '/components/journal-entry.js'
-];
+// const CACHE_NAME = 'lab7-cache';
+// const urlsToCache = [
+//   '/',
+//   '/style.css',
+//   '/scripts/script.js',
+//   '/scripts/router.js',
+//   '/components/entry-page.js',
+//   '/components/journal-entry.js'
+// ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache); 
-      })
-  );
-});
-//   - One for activation ( check out MDN's clients.claim() for this step )
-self.addEventListener('activate', function(event) {
-    event.waitUntil(clients.claim());
-    const cacheAllowlist = ['lab7-cache'];
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache); 
+//       })
+//   );
+// });
+// //   - One for activation ( check out MDN's clients.claim() for this step )
+// self.addEventListener('activate', function(event) {
+//     event.waitUntil(clients.claim());
+//     const cacheAllowlist = ['lab7-cache'];
   
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheAllowlist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    ); 
-  }); /* */
+//     event.waitUntil(
+//       caches.keys().then(function(cacheNames) {
+//         return Promise.all(
+//           cacheNames.map(function(cacheName) {
+//             if (cacheAllowlist.indexOf(cacheName) === -1) {
+//               return caches.delete(cacheName);
+//             }
+//           })
+//         );
+//       })
+//     ); 
+//   }); /* */
 
 //   - One for fetch requests
 /*
@@ -57,36 +57,36 @@ self.addEventListener('fetch', function(event) {
 }); /* */
 
 //
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then(function(response) {
+//           // Cache hit - return response
+//           if (response) {
+//             return response;
+//           }
   
-          return fetch(event.request).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
+//           return fetch(event.request).then(
+//             function(response) {
+//               // Check if we received a valid response
+//               if(!response || response.status !== 200 || response.type !== 'basic') {
+//                 return response;
+//               }
   
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
+//               // IMPORTANT: Clone the response. A response is a stream
+//               // and because we want the browser to consume the response
+//               // as well as the cache consuming the response, we need
+//               // to clone it so we have two streams.
+//               var responseToCache = response.clone();
   
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
+//               caches.open(CACHE_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request, responseToCache);
+//                 });
   
-              return response;
-            }
-          );
-        })
-      );
-  }); /* */
+//               return response;
+//             }
+//           );
+//         })
+//       );
+//   }); /* */
