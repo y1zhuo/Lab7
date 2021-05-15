b8732236d74ff9eb9866c4ec67b160c8006be2c0
Yue Zhuo <y1zhuo@ucsd.edu>
2021-05-15 15:33:37 -0700

    Lab7

--- scripts/script.js
+++ scripts/script.js
@@ -60,14 +60,14 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // register service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('./sw.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
